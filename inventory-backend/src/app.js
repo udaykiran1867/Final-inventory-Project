8c849d0e2f48c85bb1a070c1bba86a100408c373
DeekshaShettyR <161939524+DeekshaShettyR@users.noreply.github.com>
2026-01-30 10:19:55 +0530

    Update app.js

--- inventory-backend/src/app.js
+++ inventory-backend/src/app.js
@@ -32,5 +32,5 @@ app.use('/logs', logRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Backend running on port ${PORT}`);
+  console.log(` Backend running on port ${PORT}`);
 });
